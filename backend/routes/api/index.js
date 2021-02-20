acd4b31c3be8250a2ad144452a556bf5a56b3747
Hero <heroroku@gmail.com>
2021-02-20 16:30:02 -0500

    userAuth middleware complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,34 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
 
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.get(
+  '/set-token-cookie',
+  asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+    setTokenCookie(res, user);
+    return res.json({ user });
+  }));
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body })
 })

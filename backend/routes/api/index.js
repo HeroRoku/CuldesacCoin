114ba2006b98483ad4f037a1c68177411716d0e0
Hero <heroroku@gmail.com>
2021-02-21 11:22:37 -0500

    frontend setup complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -34,8 +34,8 @@ router.use('/users', usersRouter)
 //     return res.json(req.user);
 //   }
 // );
-// router.post('/test', function (req, res) {
-//   res.json({ requestBody: req.body })
-// })
+router.post('/test', function (req, res) {
+  res.json({ requestBody: req.body })
+})
 
 module.exports = router;

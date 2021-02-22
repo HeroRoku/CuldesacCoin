6b1fb2cceb27958574657e046107fa12a761d35f
Hero <heroroku@gmail.com>
2021-02-21 20:35:17 -0500

    heroku setup complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,33 +9,33 @@ const usersRouter = require('./users')
 router.use('/session', sessionRouter)
 router.use('/users', usersRouter)
 
-// router.get(
-//   '/set-token-cookie',
-//   asyncHandler(async (req, res) => {
-//     const user = await User.findOne({
-//       where: {
-//         username: 'Demo-lition'
-//       },
-//     })
-//     setTokenCookie(res, user);
-//     return res.json({ user });
-//   }));
-// router.get(
-//   '/restore-user',
-//   restoreUser,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-router.post('/test', function (req, res) {
-  res.json({ requestBody: req.body })
-})
+router.get(
+  '/set-token-cookie',
+  asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+    setTokenCookie(res, user);
+    return res.json({ user });
+  }));
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+// router.post('/test', function (req, res) {
+//   res.json({ requestBody: req.body })
+// })
 
 module.exports = router;
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -31,7 +31,7 @@ function ProfileButton({ user }) {
   return (
     <>
       <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
+        <i className="fas fa-user" />
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
